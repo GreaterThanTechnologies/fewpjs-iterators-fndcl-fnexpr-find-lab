837c5df4f54086d1286afc3dc869856cc25c55fd
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-25 21:44:26 -0400

    commit tree c1547ba07df642e3f103e7627fa46c189902b20d

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+superbowlWin = function(record) {
+  console.log(result)
 
+  let result = record.find( record => record.result === "W" )
+  console.log(result)
+
+  return !!result ? result.year : undefined
+  console.log(result)
+}
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, -2, -5, -4]
+// console.log(arr)
+// function filterPositive(num) {
+//   if (num > 0)
+//     return (num)
+
+// }
+// document.write(arr)
+// let result = arr.find(filterPositive)
+// document.write(result)
+
+
+
+
+
+
+
+
+
+
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+] 
